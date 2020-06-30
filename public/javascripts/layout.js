e987e4eac06602bae2a486762c1460d5b699cdce
solitairecreations <samrandaofficial@gmail.com>
2020-06-30 19:06:08 -0400

    Created mobile-first nav

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -7,12 +7,20 @@ let profile, submenu
 // init function
 function init() {
   // dom objects
-  profile = document.getElementById("profile")
-  submenu = document.getElementById("submenu")
+  profile = document.getElementById("nav-profile")
+  wrapper = document.getElementById("nav-wrapper")
+  submenu = document.getElementById("nav-submenu")
+  arrow = document.getElementById("nav-arrow")
 
   // event listeners
-  profile.addEventListener("mouseover", showDropdown)
-  profile.addEventListener("mouseleave", hideDropdown)
+  if (profile) {
+    wrapper.addEventListener("mouseover", showDropdown)
+    wrapper.addEventListener("mouseleave", hideDropdown)
+    arrow.addEventListener("mouseover", showDropdown)
+    arrow.addEventListener("mouseleave", hideDropdown)
+  } else {
+    arrow.style.display = "none"
+  }
 
   // this feels a bit hack-y, but it works for now
   submenu.addEventListener("mouseover", showDropdown)
@@ -22,9 +30,11 @@ function init() {
 // shows the dropdown
 function showDropdown() {
   submenu.className = "active"
+  arrow.innerHTML = "▲"
 }
 
 // hides the dropdown
 function hideDropdown() {
   submenu.className = "inactive"
+  arrow.innerHTML = "▼"
 }
