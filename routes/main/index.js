525610f7eef97da5c5a88f698c638fd6c60fdcd4
solitairecreations <samrandaofficial@gmail.com>
2020-07-02 09:35:14 -0400

    Created standalone login page

--- routes/main/index.js
+++ routes/main/index.js
@@ -2,41 +2,6 @@
 const express = require('express')
 const router = express.Router()
 
-const sql = require('../../private/javascripts/db')
-
-// handle login requests
-router.post('/', (req, res) => {
-  // assign form information to variables
-  const {username, password} = req.body
-
-  // make sure username and password fit criteria for login
-  const validity = sql.validity(username, password, password)
-  if (validity.result == true) {
-    // attempt logging in
-    sql.login(username, password, (attempt) => {
-      if (attempt.success == true) {
-        // successful login
-        console.log("successfully logged in to account '" + username + "'")
-
-        // send jwt to user
-        res.cookie('jwtToken', attempt.jwt, {maxAge: 900000, httpOnly: true, domain: 'hmpg.io'})
-      } else {
-        // username or password didn't match any database entries
-        console.log("username or password is incorrect")
-      }
-
-      // redirect back to index
-      res.redirect('/')
-    })
-  } else {
-    // username or password isn't valid for login
-    console.log("username or password is incorrect")
-
-    // redirect back to index
-    res.redirect('/')
-  }
-})
-
 // get & render page
 router.all('/', function(req, res, next) {
   req.info.title = "hmpg:home"
--- routes/mainrouter.js
+++ routes/mainrouter.js
@@ -6,7 +6,7 @@ const router = express.Router()
 const hash = require('../private/javascripts/hash')
 
 // list of available routes
-const routes = ['register', 'upload', 'files', 'account', 'settings', 'logout', '']
+const routes = ['login', 'register', 'upload', 'files', 'account', 'settings', 'logout', '']
 
 // route static pages, thank you kingsley solomon!
 for(let i = 0; i < routes.length; i++) {
