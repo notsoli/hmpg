753cdfe97e0e09104e65153c130ff98b10139235
solitairecreations <samrandaofficial@gmail.com>
2020-07-02 00:31:04 -0400

    Protected user-only pages

--- routes/main/account.js
+++ routes/main/account.js
@@ -6,8 +6,14 @@ const sql = require('../../private/javascripts/db')
 
 // get & render page
 router.all('/account', function(req, res, next) {
-  req.info.title = "hmpg:account"
-  res.render('./main/account', req.info)
+  // check if user is signed in
+  if (req.info.user) {
+    req.info.title = "hmpg:account"
+    res.render('./main/account', req.info)
+  } else {
+    // redirect back to index
+    return res.status(200).redirect('/')
+  }
 })
 
 module.exports = router
--- routes/main/settings.js
+++ routes/main/settings.js
@@ -1,4 +1,4 @@
-// account router
+// settings router
 const express = require('express')
 const router = express.Router()
 
@@ -6,8 +6,14 @@ const sql = require('../../private/javascripts/db')
 
 // get & render page
 router.all('/settings', function(req, res, next) {
-  req.info.title = "hmpg:settings"
-  res.render('./main/settings', req.info)
+  // check if user is signed in
+  if (req.info.user) {
+    req.info.title = "hmpg:settings"
+    res.render('./main/settings', req.info)
+  } else {
+    // redirect back to index
+    return res.status(200).redirect('/')
+  }
 })
 
 module.exports = router
