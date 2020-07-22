77e924722570dd624f8777c21912674b9ccfc92e
solitairecreations <samrandaofficial@gmail.com>
2020-07-22 18:43:30 -0400

    Added truncate scripts

---
+++ utils/truncateUsers.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql')
+
+// set up prompt
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+// set up sql connection
+const config = {
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "hmpg"
+}
+
+const sql = mysql.createConnection(config)
+
+// ask for confirmation
+readline.question("Are you sure you want to truncate the userinfo table? (y/N): ", (input) => {
+  readline.close()
+  if (input === "y" || input === "Y") {
+    sql.query("TRUNCATE TABLE userinfo", (err, result) => {
+      if (err) throw err
+      console.log("Successfully truncated userinfo")
+      process.exit()
+    })
+  } else {
+    console.log("Truncate aborted")
+  }
+})
